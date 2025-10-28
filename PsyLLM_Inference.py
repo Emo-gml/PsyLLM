80032481bcae4dc9b87ef1f3cd03e7e73b222f4a
HH <huhe@gml.ac.cn>
2025-10-28 15:05:54 +0800

    Update PsyLLM_Inference.py

--- PsyLLM_Inference.py
+++ PsyLLM_Inference.py
@@ -1,1 +1,44 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_path = "GMLHUHE/PsyLLM"
+
+# load the tokenizer and the model
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    torch_dtype="auto",
+    device_map="auto"
+)
+
+# prepare the model input
+prompt = "I have participated in big group sessions before where I was left to find my own safe place, but it hasn't worked for me."
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True,
+    enable_thinking=True 
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+# conduct text completion
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=32768
+)
+output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
+
+# parsing thinking content
+try:
+    index = len(output_ids) - output_ids[::-1].index(151668)
+except ValueError:
+    index = 0
+
+thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
+content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+print("PsyLLM thinking content:", thinking_content)
+print("PsyLLM content:", content)
 
